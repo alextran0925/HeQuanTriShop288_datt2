7b05be335e142bcf5b9839f6cf6b1c36badd191c
alextran0925 <trandatcdt@gmail.com>
2024-01-04 21:03:01 +0700

    main

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
-const Dia_chi_Dich_vu='http://127.0.0.1:8080';
+const Dia_chi_Dich_vu='https://services-js288.onrender.com';
+//const Dia_chi_Dich_vu='http://127.0.0.1:8080';
 //const Dia_chi_Img='http://127.0.0.1:8080';
 const Dia_chi_Img=`https://res.cloudinary.com/dzosl57og/image/upload/v1/images`;
 
@@ -28,6 +29,36 @@ const apiDienthoai=()=>{
             Ket_qua(Du_lieu)
         }
         let Tham_so = `dsDienthoai`
+        let Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}/${Tham_so}`
+        Xu_ly_HTTP.open("GET", Dia_chi_Xu_ly)
+        Xu_ly_HTTP.send()
+    })
+}
+const apiTivi=()=>{
+    return new Promise((Ket_qua, Loi) => {
+        let Du_lieu = {}
+        let Xu_ly_HTTP = new XMLHttpRequest()
+        Xu_ly_HTTP.onload = () => {
+            var Chuoi_JSON = Xu_ly_HTTP.responseText
+            Du_lieu = JSON.parse(Chuoi_JSON)
+            Ket_qua(Du_lieu)
+        }
+        let Tham_so = `dsTivi`
+        let Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}/${Tham_so}`
+        Xu_ly_HTTP.open("GET", Dia_chi_Xu_ly)
+        Xu_ly_HTTP.send()
+    })
+}
+const apiMathang=()=>{
+    return new Promise((Ket_qua, Loi) => {
+        let Du_lieu = {}
+        let Xu_ly_HTTP = new XMLHttpRequest()
+        Xu_ly_HTTP.onload = () => {
+            var Chuoi_JSON = Xu_ly_HTTP.responseText
+            Du_lieu = JSON.parse(Chuoi_JSON)
+            Ket_qua(Du_lieu)
+        }
+        let Tham_so = `dsMathang`
         let Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}/${Tham_so}`
         Xu_ly_HTTP.open("GET", Dia_chi_Xu_ly)
         Xu_ly_HTTP.send()
